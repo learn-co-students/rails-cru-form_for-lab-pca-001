1e0fe3e57b92508cad2278d96119bc959ad551cb
Aaron Wilson <adub65@gmail.com>
2020-06-15 13:20:29 -0400

    Used resource generator to create tables, models, controllers, and routes. Filled in actions and views

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,30 @@
+class GenresController < ApplicationController
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    show
+  end
+
+  def update
+    show
+    @genre.update(genre_params(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  private
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
+  end
+end
